e443d360497fd249ba3556462b99934946e6c5bf
viveknaidu007 <vivek.pavankalyan@gmail.com>
2024-04-17 13:15:23 +0530

    updated

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # repo-69-predicting-text-similarity
-here im building a model to find the text-similarity and also deployment in aws
+here im building a model to find the text-similarity of sentences and experimenting their approaches and also deployment in aws
+
+#we can see docuemntation for word embedding belowe resources
+https://radimrehurek.com/gensim/models/word2vec.html
+
+#to use pretrained models:
+https://github.com/nishankmahore/word2vec-flask-api
+
+#for huggingface model to use:
+https://huggingface.co/fse/word2vec-google-news-300
+
+#for references :
+https://github.com/PradipNichite/Youtube-Tutorials/blob/main/GPT_3_Embeddings_Youtube_.ipynb
+
+#form spacy models:
+https://spacy.io/models/en   , #download en_core_web_lg to use
+!python -m spacy download en_core_web_lg
+
+
+#for installing requiremnts
+pip install -r requiremnts.txt
+
+#to run th ecode
+python app.py
---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import spacy
+
+# now we are loading spaCy model
+nlp = spacy.load('en_core_web_lg')
+
+# loading the trained model
+model = joblib.load('semantic_similarity_model.pkl')
+
+app = Flask(__name__)
+
+# function to preprocess the text data
+def preprocess_text(text):
+    return nlp(text)
+
+# function to calculate similarity score
+def predict_similarity(text1, text2):
+    # preprocessing text
+    text1 = preprocess_text(text1)
+    text2 = preprocess_text(text2)
+    
+    # calculate similarity score using the model we trained
+    similarity_score = model.predict([[text1.similarity(text2)]])[0]
+    similarity_score = float(similarity_score)    
+    return similarity_score
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # getting data from request
+    data = request.get_json()
+    text1 = data['text1']
+    text2 = data['text2']
+    
+    # predicting similarity score
+    similarity_score = predict_similarity(text1, text2)
+    
+    # format response
+    response = {'similarity_score': similarity_score}
+    
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
